21d519fef408f96c04b3452587a790a4a89cef2c
vaishnavipatil81 <vaishnavipatil2226@gmail.com>
2025-07-18 02:47:55 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,92 @@
+x = 32
+y = 67.7
+sum=x+y
+print("Sum =",x+y);
+
+no1=int(input("Enter no1:"));
+no2=int(input("Enter no2:"));
+no3=int(input("Enter no3:"));
+no4=no1+no2;
+print("Sum =",no4);
+total=no1+no2+no3;
+print("Total sum = ",total);
+
+no5=int(input("Enter no1:"));
+no6=int(input("Enter no2:"));
+sum=no5+no6;
+print("Sum =",sum);
+
+no7=int(input("Enter no1:"));
+no8=int(input("Enter no2:"));
+multiplication=no1 * no2;
+print(multiplication)
+
+no1=int(input("Enter no1:"));
+no2=int(input("Enter no2:"));
+division=no1/no2;
+print(division);
+
+no1=float(input("Enter no1:"));
+no2=float(input("Enter no2:"));
+sub=no1-no2;
+print("sub =",sub);
+
+a=78
+b=98
+c=a%b
+print("modulus= ",c)
+
+print(type(78));
+print(type(56.89));
+
+#logical operators
+print(True and True)
+print(False and True)
+print(True and False)
+print(False and False)
+print(True or False)
+print(not True)
+
+x=True;
+y=False;
+print(x and y)
+print(x or y)
+print(not x)
+
+
+s=90
+t=87
+if s > t:
+   print("s is greater than t ");
+elif s < t :
+   print("s is greater than t");
+else :
+    print("s and t are equal");
+print((s>t) and (s<t));
+print((s>t) or (s<t));
+print( not (s>t));
+
+
+
+
+
+#comparision operators
+
+q=67.70
+r=67.90
+if q > r:
+ print("q is greater than r ");
+elif q < r :
+  print("r is greater than q");
+else :
+   print("q and r are equal");
+
+
+
+
+
+
+
+
+
+
